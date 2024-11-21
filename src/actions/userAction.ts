6f57ecc358db43f213c0e0b5bfdd3ac9b13b80fa
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-11-21 18:21:00 +0300

    change

--- src/actions/userAction.ts
+++ src/actions/userAction.ts
@@ -10,28 +10,3 @@ export async function createUser(data: User) {
     return {error}
   }
 }
-export async function getUserById({id,clerkId}: {id?: number, clerkId?: string}) {
-  try {
-    if (!id && !clerkId) {
-      throw new Error('id and clerkId is reqired')
-    }
-    const query = id ? { id } : { clerkId }
-    
-    const user = await prisma.user.findUnique({ where: query })
-    return {user}
-  } catch (error) {
-    return {error}
-  }
-}
-export async function updateUser(id: number, data: Partial<User>
-) {
-  try {
-    const user = await prisma.user.update({
-      where: {id},
-      data
-    })
-    return {user}
-  } catch (error) {
-    return {error}
-  }
-}
