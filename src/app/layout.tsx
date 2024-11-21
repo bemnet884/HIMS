4218dcf7c820ff39ab3482f267e9932ccec91370
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-11-21 20:43:31 +0300

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,26 +16,6 @@ const eb_garamond = EB_Garamond({
 export const metadata: Metadata = {
   title: "HIMS - Inventory Management System",
   description: "HIMS is a powerful inventory management system designed to streamline stock control, sales, and reporting for businesses of all sizes.",
-  keywords: "inventory management, stock control, sales tracking, business software, product management, HIMS",
-  viewport: "width=device-width, initial-scale=1.0",
-  themeColor: "#3498db", // Optional: Add theme color for mobile browsers
-  openGraph: {
-    title: "HIMS - Inventory Management System",
-    description: "Efficiently manage your inventory with HIMS. Optimize stock levels, track sales, and generate reports to grow your business.",
-    url: "https://yourwebsite.com", // Update with your actual website URL
-
-    images: [
-      { url: "/images/og-image.jpg", width: 1200, height: 630, alt: "HIMS Dashboard Preview" }
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@yourTwitterHandle",
-    creator: "@yourTwitterHandle",
-    title: "HIMS - Inventory Management System",
-    description: "A seamless solution for managing your business inventory.",
-  }
 };
 
 export default function RootLayout({
