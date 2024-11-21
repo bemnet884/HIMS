3ed7921d004d722fa02115651ec7981caec26284
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-11-21 20:02:33 +0300

    view

--- src/components/ViewDetailsButton.tsx
+++ src/components/ViewDetailsButton.tsx
@@ -1,47 +1,12 @@
-// components/ViewProductButton.tsx
-'use client';
+import React from 'react'
 
-import React, { useState, useEffect } from 'react';
-import { Button } from '@/components/ui/button';
-import ProductDetailModal from './ProductDetailModal';
-import { getProductById } from '@/actions/productActions';
-
-interface ViewProductButtonProps {
-  productId: number;
+const ViewDetailsButton = () => {
+  return (
+    <div>
+      ViewDetailsButton
+      ViewDetailsButton
+    </div>
+  )
 }
 
-const ViewProductButton: React.FC<ViewProductButtonProps> = ({ productId }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [product, setProduct] = useState(null);
-
-  useEffect(() => {
-    if (isModalOpen && !product) {
-      (async () => {
-        const fetchedProduct = await getProductById(productId);
-        setProduct(fetchedProduct);
-      })();
-    }
-  }, [isModalOpen, product, productId]);
-
-  const handleClose = () => {
-    setIsModalOpen(false);
-    setProduct(null);
-  };
-
-  return (
-    <>
-      <Button variant="link" onClick={() => setIsModalOpen(true)}>
-        View Details
-      </Button>
-      {isModalOpen && product && (
-        <ProductDetailModal
-          product={product}
-          isOpen={isModalOpen}
-          onClose={handleClose}
-        />
-      )}
-    </>
-  );
-};
-
-export default ViewProductButton;
+export default ViewDetailsButton
