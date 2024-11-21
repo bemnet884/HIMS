88f843e287042b51e1b1edcaa5cceb77d4ad11fa
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-11-21 20:45:10 +0300

    wel

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
     <div className="w-full flex-1 flex items-center justify-center">
       <SignUp
         afterSignOutUrl='/'
-        fallbackRedirectUrl="/welcome" forceRedirectUrl="/welcome" />
+        fallbackRedirectUrl="/dashboard" forceRedirectUrl="/dashboard" />
     </div>
   )
 }
