e32169b48d2ba9c41450d546b7fb36a36aa41dfc
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-12-04 20:32:54 +0300

    remove some style shiney

--- src/components/ShineyButton.tsx
+++ src/components/ShineyButton.tsx
@@ -22,7 +22,7 @@ export const ShineyButton = ({ children, className, href, ...props }: ShineyButt
         <ArrowRight className="size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[2px]" />
       </span>
 
-      <div className="ease-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white opacity-20 transition-all duration-500 group-hover:left-[120px]" />
+      <div className="absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white opacity-20 transition-all duration-500 group-hover:left-[120px]" />
     </Link>
   )
 }
